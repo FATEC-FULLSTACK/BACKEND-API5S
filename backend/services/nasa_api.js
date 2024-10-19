59118c8f90bbaffe3d50db356d17d065542da693
Leonardo Gazola Medeiros <leonardo.gazolamedeiros@gmail.com>
2024-10-19 12:50:52 -0300

    api nasa ok

---
+++ backend/services/nasa_api.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+const api_url = `https://power.larc.nasa.gov/api/projection/daily/point?`;
+
+/*
+ DADOS TESTES PARA COORDENADAS E TEMPO
+--------------------------------------
+
+
+
+---------------------------------------
+*/
+
+const start = "20241001";
+const end = "20241101";
+const latitude = -23.103170;
+const longitude = -45.735627;
+
+const fetchTemp = async (start, end, lat, long) => {
+    try {
+        const response = await axios.get(
+            `${api_url}start=${start}&end=${end}&latitude=${lat}&longitude=${long}&community=ag&parameters=T2M%2CPRECTOTCORR%2CRH2M&header=true&time-standard=utc&model=ensemble&scenario=ssp126`
+        );
+
+        console.log(response.data.properties);
+        return response.data;
+    } catch (error) {
+        console.error("Erro na requisição dos dados: ", error);
+    }
+};
+
+
+fetchTemp(start, end, latitude, longitude);
